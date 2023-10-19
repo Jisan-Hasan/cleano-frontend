2dccf6ae5393b23d50f80c6d00bb4c19e90f6ea4
Jisan-Hasan <jisan.jh5@gmail.com>
2023-10-19 15:40:22 +0600

    super admin profile update & change password feature added

--- src/app/(admin)/super_admin/change-password/page.tsx
+++ src/app/(admin)/super_admin/change-password/page.tsx
@@ -1,7 +1,67 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { useChangePasswordMutation } from "@/redux/api/authApi";
+import { Button, message } from "antd";
+
 const ChangePasswordPage = () => {
+    const [changePassword] = useChangePasswordMutation();
+
+    const onSubmit = async (data: any) => {
+        message.loading("Updating...");
+        try {
+            await changePassword(data).unwrap();
+            message.success("Updated Successfully");
+        } catch (error: any) {
+            message.error(error?.message || "Something went wrong");
+        }
+    };
+
     return (
         <div>
-            <h1>Change Password Page</h1>
+            <UMBreadCrumb
+                items={[
+                    {
+                        label: `super_admin`,
+                        link: `/super_admin`,
+                    },
+                ]}
+            />
+            <div className="max-w-[800px] mx-auto">
+                <h1 className="text-center mb-5">Change Password</h1>
+                <div>
+                    <Form
+                        submitHandler={onSubmit}
+                        // resolver={yupResolver(createCategorySchema)}
+                    >
+                        <div className="grid grid-cols-1 gap-2">
+                            <div>
+                                <FormInput
+                                    type="password"
+                                    name="oldPassword"
+                                    size="large"
+                                    label="Current Password"
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="password"
+                                    name="newPassword"
+                                    size="large"
+                                    label="New Password"
+                                />
+                            </div>
+                        </div>
+                        <div className="text-center my-5">
+                            <Button type="primary" htmlType="submit">
+                                Change Password
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/(admin)/super_admin/page.tsx
+++ src/app/(admin)/super_admin/page.tsx
@@ -1,7 +1,134 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { useGetUserQuery, useUpdateUserMutation } from "@/redux/api/userApi";
+import { getUserInfo } from "@/services/auth.service";
+import { Button, message } from "antd";
+
 const SuperAdminPage = () => {
+    const [updateUser] = useUpdateUserMutation();
+
+    const { role, email } = getUserInfo() as any;
+    console.log(role, email);
+
+    const { data: userInfo } = useGetUserQuery(email);
+
+    const onSubmit = async (data: any) => {
+        message.loading("Updating...");
+        try {
+            await updateUser({ email: email, body: data });
+            message.success("Profile Updated Successfully");
+        } catch (error: any) {
+            message.error(error?.message || "Something went wrong");
+        }
+    };
+
+    const defaultValues = {
+        email: userInfo?.email || "",
+        firstName: userInfo?.firstName || "",
+        lastName: userInfo?.lastName || "",
+        contractNo: userInfo?.contractNo || "",
+        houseNo: userInfo?.houseNo || "",
+        street: userInfo?.street || "",
+        city: userInfo?.city || "",
+        landmark: userInfo?.landmark || "",
+    };
+
     return (
         <div>
-            <h1>Welcome back super admin</h1>
+            <UMBreadCrumb
+                items={[
+                    {
+                        label: `${role}`,
+                        link: `/${role}`,
+                    },
+                ]}
+            />
+            <div className="max-w-[800px] mx-auto">
+                <h1 className="text-center mb-5">My Profile</h1>
+                <div>
+                    <Form
+                        submitHandler={onSubmit}
+                        defaultValues={defaultValues}
+                        // resolver={yupResolver(createCategorySchema)}
+                    >
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                            <div>
+                                <FormInput
+                                    type="email"
+                                    name="email"
+                                    size="large"
+                                    label="Email"
+                                    readonly={true}
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="text"
+                                    name="contractNo"
+                                    size="large"
+                                    label="Contract Number"
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="text"
+                                    name="firstName"
+                                    size="large"
+                                    label="First Name"
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="text"
+                                    name="lastName"
+                                    size="large"
+                                    label="Last Name"
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="text"
+                                    name="houseNo"
+                                    size="large"
+                                    label="House Number"
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="text"
+                                    name="street"
+                                    size="large"
+                                    label="Street"
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="text"
+                                    name="city"
+                                    size="large"
+                                    label="City"
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    type="text"
+                                    name="landmark"
+                                    size="large"
+                                    label="Landmark"
+                                />
+                            </div>
+                        </div>
+                        <div className="text-center my-5">
+                            <Button type="primary" htmlType="submit">
+                                Update Profile
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
+            </div>
         </div>
     );
 };
